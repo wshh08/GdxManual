95616d3d936bbd5ccd3b37b8b21dcdc5cb9fce67
wshh08 <wshh08@126.com>
2015-10-24 11:18:52 +0800

    init project

---
+++ app/src/main/java/com/wshh08/gdxmanual/GdxGame.java
@@ -0,0 +1,13 @@
+package com.wshh08.gdxmanual;
+
+import com.badlogic.gdx.Game;
+
+/**
+ * Created by wshh08 on 15-10-23.
+ */
+public class GdxGame extends Game {
+    @Override
+    public void create() {
+        setScreen(new GdxScreen());
+    }
+}
---
+++ testjava/src/main/java/com/wshh08/test/TestJava.java
@@ -0,0 +1,10 @@
+package com.wshh08.test;
+
+public class TestJava {
+    static {
+        System.out.println("Hello Static World.");
+    }
+    public static void main(String args[]) {
+        System.exit(0);
+    }
+}
